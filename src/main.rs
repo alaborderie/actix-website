3abfdb1c07256d44c3e64fd9c33021a42ce5db11
Antoine Laborderie <antoine.laborderie@gmail.com>
2022-12-09 01:56:25 +0100

    :sparkles: handle 404 errors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use actix_files::Files;
-use actix_web::HttpRequest;
-use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer};
+use actix_web::{
+    body::BoxBody,
+    dev::ServiceResponse,
+    get,
+    http::{header::ContentType, StatusCode},
+    middleware::{ErrorHandlerResponse, ErrorHandlers, Logger},
+    post, web, App, HttpRequest, HttpResponse, HttpServer, Result,
+};
 use handlebars::Handlebars;
+use serde_json::json;
 use std::env;
 
 use controllers::contact_controller;
@@ -21,6 +28,51 @@ async fn contact(_req: HttpRequest, contact_form: web::Json<ContactForm>) -> Htt
     contact_controller(contact_form.into_inner()).await
 }
 
+// Custom error handlers, to return HTML responses when an error occurs.
+fn error_handlers() -> ErrorHandlers<BoxBody> {
+    ErrorHandlers::new().handler(StatusCode::NOT_FOUND, not_found)
+}
+
+// Error handler for a 404 Page not found error.
+fn not_found<B>(res: ServiceResponse<B>) -> Result<ErrorHandlerResponse<BoxBody>> {
+    let response = get_error_response(&res, "Page not found");
+    Ok(ErrorHandlerResponse::Response(ServiceResponse::new(
+        res.into_parts().0,
+        response.map_into_left_body(),
+    )))
+}
+
+// Generic error handler.
+fn get_error_response<B>(res: &ServiceResponse<B>, error: &str) -> HttpResponse<BoxBody> {
+    let request = res.request();
+
+    // Provide a fallback to a simple plain text response in case an error occurs during the
+    // rendering of the error page.
+    let fallback = |e: &str| {
+        HttpResponse::build(res.status())
+            .content_type(ContentType::plaintext())
+            .body(e.to_string())
+    };
+
+    let hb = request
+        .app_data::<web::Data<Handlebars>>()
+        .map(|t| t.get_ref());
+    match hb {
+        Some(hb) => {
+            let data = json!({});
+            let body = hb.render("404", &data);
+
+            match body {
+                Ok(body) => HttpResponse::build(res.status())
+                    .content_type(ContentType::html())
+                    .body(body),
+                Err(_) => fallback(error),
+            }
+        }
+        None => fallback(error),
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Init logger
@@ -37,6 +89,7 @@ async fn main() -> std::io::Result<()> {
     let port = env::var("PORT").unwrap_or_else(|_| "8080".into());
     HttpServer::new(move || {
         App::new()
+            .wrap(error_handlers())
             .app_data(handlebars_ref.clone())
             .wrap(Logger::new(
                 "%a \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %T",
